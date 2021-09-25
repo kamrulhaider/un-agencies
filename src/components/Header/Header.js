67b5c7890a5350a369e434fabac5b732e3556dee
kamrulhaider <kamrulhaider.ssb@gmail.com>
2021-09-26 01:14:48 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Donate to UN Agencies</h1>
+            <p>
+                The United Nations is part of the UN system, which, in addition to the UN itself, comprises many funds, programmes and specialized agencies, each of which have their own area of work, leadership and budget.
+            </p>
+            <h2>Total Budget: 2000 Dollar</h2>
+        </div>
+    );
+};
+
+export default Header;
